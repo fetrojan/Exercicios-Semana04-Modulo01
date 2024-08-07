21626f74fe43d7e80b1570e047ea24d2d4818e26
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-08-07 19:46:13 -0300

    criado script JS

---
+++ script.js
@@ -0,0 +1,13 @@
+function somarNumeros (a, b) {
+    let soma = a + b
+    console.log(soma)
+}
+
+var inputNum1 = document.getElementById('inputNumero1')
+var inputNum2 = document.getElementById('inputNumero2')
+var calculadora = document.getElementById('calculadora')
+var resultado = document.getElementById('resultado')
+
+function gerarResultado {
+
+}
