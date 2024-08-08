4d1704c212f91bacf92ffc3136a3a5cc69a3bc11
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-08-08 14:40:19 -0300

    Separando em operações e finalizando layout

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ function somarNumeros (a, b) {
     return a + b
 }
 
+function subtrairNumeros (a, b) {
+    return a - b
+}
+
+function multiplicarNumeros (a, b) {
+    return a * b
+}
+
+function dividirNumeros (a, b) {
+    return a / b
+}
+
 var inputNum1 = document.getElementById('inputNumero1')
 var inputNum2 = document.getElementById('inputNumero2')
-var btnCalcular = document.getElementById('calcular')
+var btnSomar = document.getElementById('somar')
+var btnSubtrair = document.getElementById('subtrair')
+var btnMultiplicar = document.getElementById('multiplicar')
+var btnDividir = document.getElementById('dividir')
 var calculadora = document.getElementById('calculadora')
 var resultado = document.getElementById('resultado')
 
@@ -13,14 +28,34 @@ function gerarResultado (event) {
 
     var num1 = parseFloat(inputNum1.value)
     var num2 = parseFloat(inputNum2.value)
+    var operacao = event.target.id
+    var resultadoOperacao
 
-    var soma = somarNumeros(num1, num2)
-    console.log(`A soma de ${num1} com ${num2} é igual a ${soma}`)
-    resultado.innerText = `A soma de ${num1} com ${num2} é igual a ${soma}`
+    switch (operacao) {
+        case 'somar':
+            resultadoOperacao = somarNumeros(num1, num2)
+            break;
+        case 'subtrair':
+            resultadoOperacao = subtrairNumeros(num1, num2)
+            break;
+        case 'multiplicar':
+            resultadoOperacao = multiplicarNumeros(num1, num2)
+            break;
+        case 'dividir':
+            resultadoOperacao = dividirNumeros(num1, num2)
+            break;
+        default:
+            resultadoOperacao = '';
+    }
+
+    resultado.innerText = `O resultado da operação é ${resultadoOperacao}`
 
     inputNum1.value = ''
     inputNum2.value = ''
 
 }
 
-btnCalcular.addEventListener('click', gerarResultado)
+btnSomar.addEventListener('click', gerarResultado)
+btnSubtrair.addEventListener('click', gerarResultado)
+btnMultiplicar.addEventListener('click', gerarResultado)
+btnDividir.addEventListener('click', gerarResultado)
