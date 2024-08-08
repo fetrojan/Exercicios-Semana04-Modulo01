f9628ff161a41ce178e9b639bf846ac0054662a0
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-08-07 21:24:51 -0300

    criada a funcao de gerarResultado

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function somarNumeros (a, b) {
-    let soma = a + b
-    console.log(soma)
+    return a + b
 }
 
 var inputNum1 = document.getElementById('inputNumero1')
 var inputNum2 = document.getElementById('inputNumero2')
+var btnCalcular = document.getElementById('calcular')
 var calculadora = document.getElementById('calculadora')
 var resultado = document.getElementById('resultado')
 
-function gerarResultado {
+function gerarResultado (event) {
+    event.preventDefault()
+
+    var num1 = parseFloat(inputNum1.value)
+    var num2 = parseFloat(inputNum2.value)
+
+    var soma = somarNumeros(num1, num2)
+    console.log(`A soma de ${num1} com ${num2} é igual a ${soma}`)
+    resultado.innerText = `A soma de ${num1} com ${num2} é igual a ${soma}`
+
+    inputNum1.value = ''
+    inputNum2.value = ''
 
 }
+
+btnCalcular.addEventListener('click', gerarResultado)
